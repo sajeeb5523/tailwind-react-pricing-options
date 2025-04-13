c45b1bfea0aa8967a442cd652f0b930765442929
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-04-13 16:04:06 +0600

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const navigationData = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/"
+    },
+    {
+      id: 2,
+      name: "About us",
+      path: "/about-us"
+    },
+    {
+      id: 3,
+      name: "Services",
+      path: "/services"
+    },
+    {
+      id: 4,
+      name: "Contact",
+      path: "/contact"
+    },
+    {
+      id: 5,
+      name: "Blog",
+      path: "/blog"
+    }
+  ];
+  
+
+const Navbar = () => {
+    return (
+        <nav>
+
+            <ul className='flex'>
+                {
+                    navigationData.map(route =><li className='mr-10'>
+                        <a href={route.path}>{route.name}</a>
+                        </li>)
+                }
+            </ul>
+
+
+
+
+
+
+
+
+            {/* <ul className='flex'>
+                <li className='mr-10'><a href="/">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+            </ul> */}
+            
+        </nav>
+    );
+};
+
+export default Navbar;
