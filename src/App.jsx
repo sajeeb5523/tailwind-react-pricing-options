2b57d51e7726f7bb9c2d5d58f424671f9baf35c2
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-04-16 21:41:34 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import DaisyNav from './components/DaisyNav/DaisyNav'
 import Navbar from './components/Navbar/Navbar'
 import PricingOption from './components/PricingOption/PricingOption'
 import ResultChart from './components/ResultChart/ResultChart'
+import axios from 'axios'
+import MarksChart from './components/MarksChart/MarksChart'
 
-
-const pricingPromise = fetch('pricingData.json').then(res => res.json())
-
+const pricingPromise = fetch('pricingData.json').then(res => res.json());
+const marksPromise = axios.get('marksData.json');
 
 
 function App() {
@@ -23,6 +24,11 @@ function App() {
         <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
           <PricingOption pricingPromise={pricingPromise}></PricingOption>
         </Suspense>
+
+        <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
+          <MarksChart marksPromise={marksPromise}></MarksChart>
+        </Suspense>
+
 
         <ResultChart></ResultChart>
       </main>
